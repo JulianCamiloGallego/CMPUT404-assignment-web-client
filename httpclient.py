859762b80e13ac6c1498a5beb13eb85451f01af6
Julian <jcgallego22@gmail.com>
2023-10-12 14:57:21 -0600

    Update httpclient.py

--- httpclient.py
+++ httpclient.py
@@ -17,10 +17,9 @@
 import sys
 import socket
 import re
-# you may use urllib to encode data appropriately
 import urllib.parse
 
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 1024
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -36,32 +35,37 @@ class HTTPResponse(object):
 
 class HTTPClient(object):
     def connect(self, host, port):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.settimeout(10)
-        self.socket.connect((host, port))
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.settimeout(10)
+            self.socket.connect((host, port))
+        except ConnectionRefusedError:
+            return HTTPResponse(503, "Service Unavailable")
 
     def getCode(self, data):
-        return int(data.split()[1])
-
-    def getHeaders(self, data):
-        headers, _ = data.split('\r\n\r\n', 1)
-        return headers
+        # The following function from chatGPT, "How can I check that the response returned is in the correct format?", 2023-10-12
+        match = re.match(r'HTTP/\d\.\d (\d+) (.*)', data)
+        if match:
+            return int(match.group(1))
+        return -1
 
     def getBody(self, data):
         _, body = data.split('\r\n\r\n', 1)
         return body
     
     def sendall(self, data):
-        self.socket.sendall(data.encode('utf-8'))
+        if self.socket:
+            self.socket.sendall(data.encode('utf-8'))
         
     def close(self):
-        self.socket.close()
+        if self.socket:
+            self.socket.close()
 
     def recvall(self, sock):
         buffer = bytearray()
         done = False
         while not done:
-            part = sock.recv(1024)
+            part = sock.recv(BUFFER_SIZE)
             if (part):
                 buffer.extend(part)
             else:
@@ -69,67 +73,66 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        try:
-            parsedUrl = urllib.parse.urlparse(url)
-            host = parsedUrl.hostname
-            port = parsedUrl.port if parsedUrl.port else 80
-            path = parsedUrl.path if parsedUrl.path else '/'
+        parsedUrl = urllib.parse.urlparse(url)
+        host = parsedUrl.hostname
+        port = parsedUrl.port if parsedUrl.port else 80
+        path = parsedUrl.path if parsedUrl.path else '/'
 
-            if args:
-                path += '?' + urllib.parse.urlencode(args)
+        req = f"GET {path} HTTP/1.1\r\n"
+        req += f"Host: {host}\r\n"
+        req += f"Content-Length: 0\r\n"
+        req += "Connection: close\r\n\r\n"
 
-            req = f"GET {path} HTTP/1.1\r\n"
-            req += f"Host: {host}\r\n"
-            req += "Connection: close\r\n\r\n"
+        self.connect(host, port)
+        self.sendall(req)
 
-            self.connect(host, port)
-            self.sendall(req)
+        res = self.recvall(self.socket)
+        self.close()
 
-            res = self.recvall(self.socket)
-            self.close()
+        code = self.getCode(res)
 
-            code = self.getCode(res)
-            body = self.getBody(res)
+        if (code == -1):
+            return HTTPResponse(500, "Invalid response format")
 
-            return HTTPResponse(code, body)
-        
-        except ConnectionRefusedError:
-            return HTTPResponse(503, "Service Unavailable")
+        body = self.getBody(res)
+
+        return HTTPResponse(code, body)        
 
     def POST(self, url, args=None):
-        try:
-            parsedUrl = urllib.parse.urlparse(url)
-            host = parsedUrl.hostname
-            port = parsedUrl.port if parsedUrl.port else 80
-            path = parsedUrl.path if parsedUrl.path else '/'
+        parsedUrl = urllib.parse.urlparse(url)
+        host = parsedUrl.hostname
+        port = parsedUrl.port if parsedUrl.port else 80
+        path = parsedUrl.path if parsedUrl.path else '/'
 
-            if args:
-                body = urllib.parse.urlencode(args)
-                contentLength = len(body)
-            else:
-                body = ""
-                contentLength = 0
+        if args:
+            body = urllib.parse.urlencode(args)
+            contentLength = len(body)
+        else:
+            body = ""
+            contentLength = 0
 
-            req = f"POST {path} HTTP/1.1\r\n"
-            req += f"Host: {host}\r\n"
-            req += f"Content-Type: application/x-www-form-urlencoded\r\n"
-            req += f"Content-Length: {contentLength}\r\n"
-            req += "Connection: close\r\n\r\n"
-            req += body
+        req = f"POST {path} HTTP/1.1\r\n"
+        req += f"Host: {host}\r\n"
+        req += f"Content-Type: application/x-www-form-urlencoded\r\n"
+        req += f"Content-Length: {contentLength}\r\n"
+        req += "Connection: close\r\n\r\n"
+        req += body
 
-            self.connect(host, port)
-            self.sendall(req)
+        self.connect(host, port)
+        self.sendall(req)
 
-            res = self.recvall(self.socket)
-            self.close()
+        res = self.recvall(self.socket)
+        self.close()
 
-            code = self.getCode(res)
-            body = self.getBody(res)
+        code = self.getCode(res)
 
-            return HTTPResponse(code, body)
-        
-        except ConnectionRefusedError:
-            return HTTPResponse(503, "Service Unavailable")
+        if (code == -1):
+            return HTTPResponse(500, "Invalid response format")
+
+        body = self.getBody(res)
+
+        return HTTPResponse(code, body)
+
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
